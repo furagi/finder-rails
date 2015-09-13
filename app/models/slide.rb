eb2c675a8c8513dc9c3347cb5b50b8ff7535ecaa
furagi <furagi.usagi@gmail.com>
2015-09-13 16:38:50 +1000

    slides rendering (JSON and css at fromtpage) was fixed

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -5,4 +5,10 @@ class Slide < ActiveRecord::Base
     content_type: { content_type: "image/jpeg" },
     size: { in: 50..5120.kilobytes }
   validates :image_fingerprint, :uniqueness => { :message => "Image has already been uploaded." }
+
+  def as_json(options = nil)
+    slide = super({ only: [:id, :image_content_type, :image_file_name, :image_file_size] }.merge(options || {}))
+    slide[:url] = self.image.url
+    slide
+  end
 end
