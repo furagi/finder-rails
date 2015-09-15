b8715149f4a5e8e0a9be444d57d2a0792a8dbfb4
furagi <furagi.usagi@gmail.com>
2015-09-15 17:20:47 +1000

    little fix in migration

--- db/migrate/20150825145445_rename_main_photo_to_main_photo_id.rb
+++ db/migrate/20150825145445_rename_main_photo_to_main_photo_id.rb
@@ -1,5 +1,6 @@
 class RenameMainPhotoToMainPhotoId < ActiveRecord::Migration
   def change
+    remove_foreign_key :girls, column: :main_photo
     rename_column :girls, :main_photo, :main_photo_id
   end
 end
